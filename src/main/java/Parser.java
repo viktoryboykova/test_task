6e5b90d5d9bba9804175ba40ed017c3737c8b61c
viktoryboykova <viktoryboykova@list.ru>
2022-07-04 10:51:53 +0300

    fix Parser

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -6,23 +6,31 @@ public class Parser {
     private final Map<String, Integer> values = new HashMap<>();
 
     public Parser(String[] args) {
-        int defaultLinesNumber = 10000;
+        int defaultLinesNumber = 1000;
         int defaultLineLength = 100;
         for (String arg : args) {
             String[] words = arg.split("=");
             if (words.length != 2) {
-                throw new IllegalArgumentException("Неверный формат ввода аргумента. Образец: -key=value.");
+                throw new IllegalArgumentException("Invalid argument input format. Sample: -key=value.");
             }
             if (words[0].isEmpty() || words[1].isEmpty()) {
-                throw new IllegalArgumentException("Введен пустой ключ/значение. Образец: -key=value.");
-            }
-            if ((words[0].equals("-l") || words[0].equals("-n")) && Integer.parseInt(words[1]) < 0) {
-                throw new IllegalArgumentException("Введено отрицательное значение для ключа " + words[0]);
+                throw new IllegalArgumentException("An empty key/value is entered. Sample: -key=value.");
             }
             if (!words[0].startsWith("-")) {
-                throw new IllegalArgumentException("Ключ " + words[0] + "должен начинаться с \"-\"");
+                throw new IllegalArgumentException("Key " + words[0] + "must start with \"-\"");
             }
-            values.put(words[0].substring(1), Integer.valueOf(words[1]));
+            if ((words[0].equals("-l") || words[0].equals("-n"))) {
+                int value;
+                try {
+                    value = Integer.parseInt(words[1]);
+                    if (value < 0) {
+                        throw new IllegalArgumentException("A negative value has been entered for the key " + words[0]);
+                    }
+                } catch (NumberFormatException ex) {
+                    throw new IllegalArgumentException("A string is entered, not a number");
+                }
+                values.put(words[0].substring(1), value);
+            }
         }
         if (!values.containsKey("l")) {
             values.put("l", defaultLineLength);
